a7f72cb270d40c60427fb5ecceaadec2713ba036
Anurag <107977228+anurag-gv@users.noreply.github.com>
2025-07-16 08:30:12 +0530

    Update time_series_visualizer.py

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -5,19 +5,21 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
+df = pd.read_csv('fcc-forum-pageviews.csv', parse_dates=True, index_col=[0])
 
 # Clean data
-df = None
-
+cond1 = df.value <= df.value.quantile(97.5/100)
+cond2 = df.value >= df.value.quantile(2.5/100)
+df = df[cond1 & cond2]
 
 def draw_line_plot():
     # Draw line plot
-
-
-
-
-
+    fig, axes = plt.subplots(figsize=(16,6))
+    axes.plot(df.index, df.value, color='red')
+    axes.set_title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
+    axes.set_xlabel('Date')
+    axes.set_ylabel('Page Views')
+    
     # Save image and return fig (don't change this part)
     fig.savefig('line_plot.png')
     return fig
